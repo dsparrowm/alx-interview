d3394fe1ac2f458c795923976cc33634ad28048d
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-06-28 07:00:45 +0000

    added solutions to pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+"""This module represents the pascal triangle of n numbers"""
+
+
+def pascal_triangle(n):
+    """function that returns a list of lists of integers
+    representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+    triangle = []
+    for row_num in range(n):
+        row = []
+        for i in range(row_num + 1):
+            if i == 0 or i == row_num:
+                row.append(1)
+            else:
+                value = triangle[row_num - 1][i] + triangle[row_num - 1][i - 1]
+                row.append(value)
+        triangle.append(row)
+    return triangle
