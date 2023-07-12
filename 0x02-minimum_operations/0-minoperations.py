348110319e0ca91e6b3e078af9d3859c65129683
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-07-12 10:54:03 +0000

    added solution to minimum operations task and README file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module contains solutions to Minimum Operations interview
+   questions
+"""
+
+
+def minOperations(n):
+    """this function takes a number n and returns the minimum number
+    of operations to result in n
+    """
+    # first get the prime numbers of n
+    factors = []
+    for i in range(2, n + 1):
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+    # the sum of the prime numbers is the minimum operations
+    return sum(factors)
