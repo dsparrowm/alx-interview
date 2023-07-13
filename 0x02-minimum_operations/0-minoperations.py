601fa3f83e6392e26e225cb243da5865e25e3060
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-07-13 21:30:50 +0000

    added task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,17 +11,12 @@ def minOperations(n):
     # first get the prime numbers of n
     if n <= 1:
         return 0
-    dp = [float('inf')] * (n + 1)
-    # Base case: No operations needed for n < 2
-    dp[1] = 0
-    
-    # The outer loop iterates from 2 to n
-    for i in range(2, n + 1):
-        #  The inner loop iterates from 1 to i - 1
-        # and checks if i is divisible by j.
-        for j in range(1, i):
-            if i % j == 0:
-                # If it is, We update the minimum operations
-                # using min(dp[i], dp[j] + i // j).
-                dp[i] = min(dp[i], dp[j] + i // j)
-    return dp[n]
+    operations = 0
+    divisor = 2
+    while n > 1:
+        if n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        else:
+            divisor += 1
+    return operations
